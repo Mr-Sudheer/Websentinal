283f2fa38853b6cf5f5eedc8de5283ed15347f22
Chillakuru Sudheer <senorsudheer@gmail.com>
2026-03-16 14:29:24 +0530

    Added extraction of resources, scripts and inputs

--- Websentinal.py
+++ Websentinal.py
@@ -11,31 +11,63 @@ headers={
 }
 
 def extract_links(soup, base_url):
-    links=[]
+    links=set()
 
-    for link in soup.find_all(["a", "link"], href=True):
+    for link in soup.find_all(["a"], href=True):
         new_url=urljoin(base_url, link["href"])
         clean_url=urldefrag(new_url).url
-        links.append(clean_url)
+        links.add(clean_url)
 
     return links
 
+def extract_resources(soup, base_url):
+    resources=set()
+
+    for resource in soup.find_all("link", href=True):
+        rsc=urljoin(base_url, resource["href"])
+        resources.add(rsc)
+    
+    return resources
+
 def extract_images(soup, base_url):
-    images=[]
+    images=set()
     
     for image in soup.find_all("img", src=True):
         img_url=urljoin(base_url, image["src"])
-        images.append(img_url)
+        images.add(img_url)
 
     return images
+
+def extract_script(soup, base_url):
+    scripts=set()
+
+    for jscript in soup.find_all("script", src=True):
+        js_url=urljoin(base_url, jscript["src"])
+        scripts.add(js_url)
     
+    return scripts
+
+def extract_inputs(soup):
+    inputs=set()
+
+    for form in soup.find_all("input", type=True):
+        fname=form.get("name")
+        ftype=form.get("type")
+        inputs.add((fname, ftype))
+    return inputs
 
 def static_crawl(start_url):
     visited=set()
     queue=deque()
     queue.append((start_url,0))
 
-    max_depth=2
+    all_links=set()
+    all_rsc=set()
+    all_images=set()
+    all_scripts=set()
+    all_inputs=set()
+
+    max_depth=5
 
     while queue:
         current_url,depth=queue.popleft()
@@ -44,7 +76,6 @@ def static_crawl(start_url):
             continue
 
         visited.add(current_url)
-        print(current_url)
 
         try:
             r=requests.get(current_url, headers=headers, timeout=10)
@@ -54,18 +85,62 @@ def static_crawl(start_url):
 
             soup=BeautifulSoup(r.text,"lxml")
 
-            print("\nLinks")
-            links=extract_links(soup, current_url)
-            for link in links:
-                if link not in visited:
-                    queue.append((link, depth+1))
-                
-            print("\nImages")
-            images=extract_images(soup, current_url)
-            for image in images:
-                print(image)
+            links=extract_links(soup, current_url) #Links
 
-        except:
-            pass
+            for link in links:
+                all_links.add(link)
+                if link not in visited and link not in all_links:
+                    queue.append((link, depth+1))
+
+            resources=extract_resources(soup, current_url) #Resources
+
+            for resource in resources:
+                all_rsc.add(resource)
+
+            images=extract_images(soup, current_url) #Images
+                 
+            for image in images:
+                all_images.add(image)
+
+            script=extract_script(soup, current_url) #Script
+
+            for jscrpt in script:
+                all_scripts.add(jscrpt)
+
+            inputs=extract_inputs(soup) #inputs
+
+            for inp in inputs:
+                all_inputs.add(inp)
+
+        except Exception as e:
+            print("Error: ", e)
+
+    print("\nLinks")
+    for lnk in all_links:
+        print(lnk)
+
+    print("\nResources")
+    for rsc in all_rsc:
+        print(rsc)
+
+    print("\nImages")
+    for img in all_images:
+        print(img)
+
+    print("\nScripts")
+    for scrpt in all_scripts:
+        print(scrpt)
+
+    print("\nInputs")
+    for inpt in all_inputs:
+        print(inpt)
+    print("\n")
+
+    print("Total links: ", len(all_links))
+    print("Total images: ", len(all_images))
+    print("Total resources: ", len(all_rsc))
+    print("Total scripts: ", len(all_scripts))
+    print("Total inputs: ", len(all_inputs))
+
 
 static_crawl(url)
